a78ec88acd8832df174fe0bb27db8972e34d054e
snowgoing <snowgoing@gmail.com>
2017-03-31 17:41:09 -0700

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Mike',
-    text: 'I like being peaceful',
-    createdAt: new Date().toLocaleString()
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().toLocaleString()
+    });
   });
 
   socket.on('disconnect', () => {
